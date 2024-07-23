95c959d1a6316df64c3911db3fc7b95376be4571
Nicolas Jardim <nicolasemkis@gmail.com>
2024-07-23 22:27:06 +0100

    Update script

--- scripts/setup-bluetooth.sh
+++ scripts/setup-bluetooth.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 MAGIC_TRACKPAD_BLUETOOTH_ID=bc-d0-74-b7-a3-f7
+MAGIC_KEYBOARD_BLUETOOTH_ID=88-4d-7c-eb-a9-88
 
-echo "Pairing and Unparing devices"
-
+echo "Unparing devices..."
 blueutil --unpair $MAGIC_TRACKPAD_BLUETOOTH_ID
+blueutil --unpair $MAGIC_KEYBOARD_BLUETOOTH_ID
+
+echo "Paring devices..."
 blueutil --pair $MAGIC_TRACKPAD_BLUETOOTH_ID
 blueutil --pair $MAGIC_TRACKPAD_BLUETOOTH_ID
 
-echo "Done ✅"
+blueutil --pair $MAGIC_KEYBOARD_BLUETOOTH_ID
+blueutil --pair $MAGIC_KEYBOARD_BLUETOOTH_ID
+
+echo "Done."
