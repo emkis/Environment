f095512dc8ac9e35f1d6b37c997a9e7c25510f6d
emkis <nicolasemkis@gmail.com>
2025-03-08 13:46:52 +0000

    remove steps for pairing magic mouse

--- scripts/setup-bluetooth.sh
+++ scripts/setup-bluetooth.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 MAGIC_TRACKPAD_BLUETOOTH_ID=bc-d0-74-b7-a3-f7
-MAGIC_KEYBOARD_BLUETOOTH_ID=88-4d-7c-eb-a9-88
 
 echo "Unparing devices..."
 blueutil --unpair $MAGIC_TRACKPAD_BLUETOOTH_ID
-blueutil --unpair $MAGIC_KEYBOARD_BLUETOOTH_ID
 
 echo "Paring devices..."
 blueutil --pair $MAGIC_TRACKPAD_BLUETOOTH_ID
 blueutil --pair $MAGIC_TRACKPAD_BLUETOOTH_ID
 
-blueutil --pair $MAGIC_KEYBOARD_BLUETOOTH_ID
-blueutil --pair $MAGIC_KEYBOARD_BLUETOOTH_ID
-
-echo "Done."
+echo "Done"
