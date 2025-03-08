d049ca63d778feb9d6d402eb0eebc7be07a717ea
emkis <nicolasemkis@gmail.com>
2025-03-08 15:10:59 +0000

    add recipe for window manager

---
+++ recipes/setting-up-window-manager/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '🪟 Window manager setup started'
+
+echo '>> Installing rectangle'
+brew install --cask rectangle-pro
+
+echo '>> Opening rectangle'
+open -a "Rectangle Pro" # First time it will ask for permissions and open on the menu bar
+open -a "Rectangle Pro" # Second time it will open the app
+
+echo "🪟 Window manager setup completed, don't forget to read all instructions on the readme."
