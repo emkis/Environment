77a67ed11f5b1b10c45d858b8649c7a02d358c81
emkis <nicolasemkis@gmail.com>
2025-10-21 17:22:24 +0100

    Add script to verify sha256 file hashes

---
+++ recipes/setting-up-backups/verify-hashes.sh
@@ -0,0 +1,10 @@
+#!/opt/homebrew/bin/bash
+
+echo "🔍 Verifying file integrity against stored hashes"
+echo # Break line
+
+target_directory="$1" # Gets the target directory from the first argument
+sha256sum -c "$target_directory"/backup-checksums.txt # Compare file hashes with previously generated hashes
+
+echo # Break line
+echo "✅ Hash verification completed"
