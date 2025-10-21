99ff7c92f391ecbb0878ff71f89d0216376ab08f
emkis <nicolasemkis@gmail.com>
2025-10-21 23:22:20 +0100

    Exclude macos files from file match

--- recipes/setting-up-backups/create-hashes.sh
+++ recipes/setting-up-backups/create-hashes.sh
@@ -1,12 +1,12 @@
 #!/opt/homebrew/bin/bash
 
-echo '#️⃣ Finding files to hash'
+echo '#️⃣  Finding files to hash'
 
 target_directory="$1" # Gets the target directory from the first argument
-files=$(find "$target_directory" -type f) # Finds all files in the target directory recursively
+files=$(find "$target_directory" -type f -not -name ".DS_Store") # Finds all files in the target directory recursively, excluding .DS_Store files
 file_count=$(echo "$files" | wc -l) # Counts the number of matching files
 
-echo "#️⃣ Creating hashes for $file_count files"
+echo "#️⃣  Creating hashes for $file_count files"
 
 # Create backup-metadata directory if it doesn't exist
 # Pipe each file path into sha256sum to create its hash
