b1a448ce2d768beda73a8a870712e7a51162f70c
emkis <nicolasemkis@gmail.com>
2025-10-21 17:43:34 +0100

    Create the backup-metadata folder if not there already

--- recipes/setting-up-backups/create-hashes.sh
+++ recipes/setting-up-backups/create-hashes.sh
@@ -8,8 +8,10 @@ file_count=$(echo "$files" | wc -l) # Counts the number of matching files
 
 echo "#️⃣ Creating hashes for $file_count files"
 
+# Create backup-metadata directory if it doesn't exist
 # Pipe each file path into sha256sum to create its hash
 # Writes the created file hash to checksums.txt file
+mkdir -p "$target_directory/backup-metadata"
 echo "$files" | tr '\n' '\0' | xargs -0 sha256sum > "$target_directory"/backup-metadata/checksums.txt
 
 echo '✅ Created file hashes for all files'
