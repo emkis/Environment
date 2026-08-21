80a8f42667cc026123763475865cf9139c94452d
emkis <nicolasemkis@gmail.com>
2026-08-21 23:52:33 +0100

    Add lora font and update font checkers

--- packages/chest/manifest/font.ts
+++ packages/chest/manifest/font.ts
@@ -6,13 +6,19 @@ export const font: Entry[] = [
   {
     name: "fira-code",
     category: "font",
-    check: `ls "${FONTS_DIR}" | grep -qi "^FiraCode-"`,
+    check: `command ls "${FONTS_DIR}" | grep -qi "^FiraCode-"`,
     install: "brew install --cask font-fira-code",
   },
   {
     name: "fira-code-nerd-font",
     category: "font",
-    check: `ls "${FONTS_DIR}" | grep -qi "Fira.*Nerd"`,
+    check: `command ls "${FONTS_DIR}" | grep -qi "Fira.*Nerd"`,
     install: "brew install --cask font-fira-code-nerd-font",
+  },
+  {
+    name: "lora",
+    category: "font",
+    check: `command ls "${FONTS_DIR}" | grep -qi "^Lora"`,
+    install: "brew install --cask font-lora",
   },
 ];
